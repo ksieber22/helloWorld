1d346c20a914873db96c1ec68882bfbb445ee4c1
ksieber22 <ksieber@terpmail.umd.edu>
2024-02-20 16:33:54 -0500

    Added a route to reach the "About Me" page.

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def hello_world():  # put application's code here
 def hello():  # put application's code here
     return render_template('hello.html')
 
+
 @app.route('/about')
 def about():  # put application's code here
     return render_template('about.html')
