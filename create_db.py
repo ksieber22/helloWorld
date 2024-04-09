0774408639f70dcb1b926c2f11fe5d849749b890
ksieber22 <ksieber@terpmail.umd.edu>
2024-04-08 20:52:50 -0400

    Added myself as a user from the student view.

--- create_db.py
+++ create_db.py
@@ -19,6 +19,8 @@ with app.app_context():
     users = [
         {'username': 'student', 'email': 'student@umd.edu', 'first_name':'Imma', 'last_name':'Student',
             'password': generate_password_hash('studentpw', method='pbkdf2:sha256'), 'role':'STUDENT'},
+        {'username': 'ksieber', 'email': 'ksieber@terpmail.umd.edu', 'first_name': 'Kevin', 'last_name': 'Sieber',
+            'password': generate_password_hash('ksieber', method='pbkdf2:sha256'), 'role': 'STUDENT'},
         {'username': 'manager', 'email': 'manager@umd.edu', 'first_name':'Joe', 'last_name':'King',
             'password': generate_password_hash('managerpw', method='pbkdf2:sha256'), 'role':'MANAGER'},
         {'username': 'admin', 'email': 'admin@umd.edu', 'first_name':'Crystal', 'last_name':'Ball',
